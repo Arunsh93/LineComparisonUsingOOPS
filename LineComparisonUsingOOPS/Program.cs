9f6d221746b507fcdaeb0792411af5b9a7620001
Arunsh93 <Arunhosur93@gmail.com>
2021-09-27 13:15:59 +0530

    Pushing Main method class

--- LineComparisonUsingOOPS/Program.cs
+++ LineComparisonUsingOOPS/Program.cs
@@ -7,6 +7,9 @@ namespace LineComparisonUsingOOPS
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Program Using OOPS");
+
+            UC1_LengthCalculation obj1 = new UC1_LengthCalculation();
+            obj1.getCartician();
         }
     }
 }
